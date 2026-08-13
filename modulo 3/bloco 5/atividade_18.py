616757cc7cc8d48e1d05bf11ecd0ac04efeaaedc
hudson1208 <hudson1208>
2026-08-13 17:21:47 -0300

    questoes bloco 5 atividade 18,19,20

---
+++ modulo 3/bloco 5/atividade_18.py
@@ -0,0 +1,6 @@
+### Atividade 18 — Saudação personalizada
+
+# Crie uma função que receba o nome de uma pessoa como parâmetro
+# e apresente uma saudação personalizada. Solicite o nome ao usuário
+# e utilize-o na chamada da função.
+
---
+++ modulo 3/bloco 5/teste.py
@@ -0,0 +1,57 @@
+#  def exibir_mensagem():
+#      print("Bem-vindo ao sistema.")
+
+#  exibir_mensagem()
+
+#  ////////////////////////////////////////////////
+
+# def exibir_menu():
+#     print("1 - Cadastrar")
+#     print("2 - Consultar")
+#     print("3 - Encerrar")
+# exibir_menu()
+
+#  ////////////////////////////////////////////////////
+
+# def saudar(nome):
+#     print(f"Ola, {nome}!")
+# saudar("hudson gostoso e delicioso")
+# saudar("caludio")
+
+# ///////////////////////////////////////////
+
+# def apresentar_produtor(nome, preco, estoque):
+#     print(f"produtor: {nome}")
+#     print(f"Preço: R${preco:.2f}")
+#     print(f"Estoque: {estoque}")
+
+# apresentar_produtor(
+#     "Monitor",
+#     1000.00,
+#     5
+# )
+# /////////////////////////////////////////////////////////
+
+# def apresentar_aluno(nome, idade, nota):
+#     print(nome, idade, nota)
+    
+# apresentar_aluno(
+#     "hudson"
+#     30
+#     6,5
+# )
+
+# ////////////////////////////////////
+# aluno = {
+#     "nota": 6.5,
+#     "nome": "hudson",
+#     "idade": 30
+# }
+
+# def apresentar_aluno():
+#     for chave, valor in aluno.items():
+#         print(f"{chave}: {valor}")
+
+# apresentar_aluno()
+
+# /////////////////////////////////////////////////
